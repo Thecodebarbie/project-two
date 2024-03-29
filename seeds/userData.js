859f0073944615874e817a0ad84b40908081b4c6
Alex Silien <alexsilien64@gmail.com>
2024-03-29 16:43:21 -0400

    saved

---
+++ seeds/userData.js
@@ -0,0 +1,31 @@
+[
+    {
+        "username" : "timrob",
+        "email" : "timrobert@gmail.com",
+        "password" : "password123"
+    },
+    {
+        "username" : "robalex",
+        "email": "robalexander@hotmail.com",
+        "password" : "password123"
+    },
+    {
+        "username" : "dianajo",
+        "email" : "dianajohn@yahoo.com",
+        "password" : "password123"
+    },
+    {
+        "username" : "maryxa",
+        "email": "maryxavier@gmail.com",
+        "password" : "password123"
+    },
+    {
+        "username" : "aprilda",
+        "email" : "aprildaniel@aol.com",
+        "password" : "password123"
+    },
+    {   "username" : "paigefr",
+        "email" : "paigefrancis@yahoo-com",
+        "password" : "password123"
+    }
+]
