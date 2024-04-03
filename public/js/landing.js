acec8eeceb27e350a3c652089f5db0ad5617db5d
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-02 20:47:19 -0400

    added click event to login btn

---
+++ public/js/landing.js
@@ -0,0 +1,9 @@
+const log1 = document.getElementById('log-1')
+const log2 = document.getElementById('log-2')
+
+function goToLogin() { //redirect to login
+    location.href = '/login'
+}
+
+log1.addEventListener('click', goToLogin)
+log2.addEventListener('click', goToLogin)
