bb6b4eb66d7cf2cbb70414d433b28fdcd3961496
Alex Silien <alexsilien64@gmail.com>
2024-04-04 16:27:58 -0400

    saved

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
 const router = require("express").Router()
-const twoFactorRoutes = require("./twoFactorRoutes")
 
 apiRoutes = require("./api")
 htmlRoutes = require("./homeRoutes")
@@ -11,8 +10,5 @@ router.use("/", htmlRoutes)
 /* API routes */
 //http:localhost:3001/api
 router.use("/api", apiRoutes)
-
-// http://localhost:3001/api/authenticate
-router.use("/authenticate", twoFactorRoutes)
 
 module.exports = router
