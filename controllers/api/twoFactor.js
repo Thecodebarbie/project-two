320bfe58aa1214199a65981cf1ce94399c39d9e3
Alex Silien <alexsilien64@gmail.com>
2024-04-03 22:04:29 -0400

    saved

--- controllers/api/twoFactor.js
+++ controllers/api/twoFactor.js
@@ -1,41 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const { Employee } = require('../../models')
 
 const app = express();
 const PORT = 3001;
 
-const employee = [
-    { username: 'timrob', email: 'timrobert@gmail.com', password: 'timrob123', employeeId: '1245' },
-    { username: 'robalex', email: 'robalexander@hotmail.com', password: 'robalex123', employeeId: '1345' },
-    { username: 'dianajo', email: 'dianajohn@yahoo.com', password: 'dianajo123', employeeId: '2345' },
-    { username: 'maryxa', email: 'maryxavier@gmail.com', password:'maryxa123' ,employeeId: '1234' },
-    { username: 'aprilda', email: 'aprildaniel@aol.com', password: 'aprilda123' ,employeeId: '1254' },
-    { username: 'paigefr', email: 'paigefrancis@yahoo.com', password: 'paigefr123' ,managerId: '5413' },
-];
-
-
-app.post('/login', (req, res) => {
-    const { email, password, } =req.body;
-    const employee = employee.find(employee => employee.email === email && employee.password === password);
-    if (!employee) {
-        return res.status(401).send('Invalid username or password');
-    }
-    res.json({ promptEmployeeId: true }); 
-});
-
 app.post('/authenticate', (req, res) => {
-    const { email, employeeID } = req.body;
-    const employee = employee.find(user => user.username === username && user.employeeId === employeeId);
-    if (!user) {
+    const { EmployeeID } = req.body;
+    const employee = Employee.find(Employee => Employee.username === username && Employee.EmployeeID === EmployeeID);
+    if (!Employee) {
         return res.status(401).send('Invalid employee ID');
     }
     res.send('Thank you for using two factor authentication!');
 });
 
-app.post('/authenticate', (req, res) => {
-    const { username, managerID } = req.body;
-    const user = users.find(user => user.username === username && user.managerId === managerId);
-    if (!user) {
+app.post('/authenticate/manager', (req, res) => {
+    const { managerID } = req.body;
+    const employee = Employee.find(Employee => Employee.username === username && Employee.managerID === managerID);
+    if (!Employee) {
         return res.status(401).send('Invalid manager ID');
     }
     res.send('Thank you for using two factor authentication!');
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const twoFactorRoutes = require("./twoFactorRoutes")
 
 apiRoutes = require("./api")
 htmlRoutes = require("./homeRoutes")
@@ -10,5 +11,8 @@ router.use("/", htmlRoutes)
 /* API routes */
 //http:localhost:3001/api
 router.use("/api", apiRoutes)
+
+// http://localhost:3001/api/authenticate
+router.use("/authenticate", twoFactorRoutes)
 
 module.exports = router
